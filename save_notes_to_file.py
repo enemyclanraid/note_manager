b401c1e40646f1b94f435e7f8cbb8a768a7b1ce3
George <georgetokach@gmail.com>
2025-01-22 18:26:54 +0300

    final

--- save_notes_to_file.py
+++ save_notes_to_file.py
@@ -640,7 +640,7 @@ def main():
             search_notes(notes, keyword if keyword else None, status)
 
         elif command_input == 'delete':
-            delete_note(notes)
+            delete_note(notes) # test
 
         elif command_input == 'retry':
             if not notes:
