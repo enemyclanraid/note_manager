6f4761893ec791320aa052780c643c823e01f99e
George <georgetokach@gmail.com>
2025-01-29 13:08:14 +0300

    final

--- note_manager/interface/user_interface.py
+++ note_manager/interface/user_interface.py
@@ -381,30 +381,3 @@ def display_note_info(note):
     print("Дата истечения заметки:", note["issue_date"])
     print("Заголовки заметок:", sorted(note["titles"]))
 
-
-def create_note():
-    """Создаёт новую заметку."""
-    username = input("Введите имя пользователя: ")
-
-    titles = set()
-
-    while True:
-        title = input("Введите заголовок (или оставьте пустым для завершения): ")
-        if title == "":
-            break
-        if title in titles:
-            print("Этот заголовок уже существует. Пожалуйста, введите другой.")
-        else:
-            titles.add(title)
-
-    content = input("Введите описание заметки: ")
-
-
-    return {
-        "username": username,
-        "content": content,
-        "status": status,
-        "created_date": created_date,
-        "issue_date": issue_date,
-        "titles": list(titles)
-    }
